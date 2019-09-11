f08be4af620f71dd37793cc6560883048733ee77
Ankitagole <ankita17@navgurukul.org>
2019-09-11 16:55:36 +0530

    add. fields in vending machine class

--- src/main/java/vender/VendingMachine.java
+++ src/main/java/vender/VendingMachine.java
@@ -1,4 +1,10 @@
 package vender;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class VendingMachine {
+    Item item;
+    int capcity;
+    List<Integer> coins = Arrays.asList(1,5,110,25);
 }
