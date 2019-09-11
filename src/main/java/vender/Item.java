2a83730e5e8e435e49813666454e3809a9af8c53
Ankitagole <ankita17@navgurukul.org>
2019-09-11 16:37:00 +0530

    add. constructor and field in item class

--- src/main/java/vender/Item.java
+++ src/main/java/vender/Item.java
@@ -1,5 +1,10 @@
 package vender;
 
 public class Item {
-
+    private String name;
+    private double price;
+    public Item(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
 }
