bffc5f3a958cea7719740b07aeb864fef6cda3d0
Ankitagole <ankita17@navgurukul.org>
2019-09-12 18:17:38 +0530

    add. add method in inventory class

--- src/main/java/vender/Inventory.java
+++ src/main/java/vender/Inventory.java
@@ -3,6 +3,9 @@ package vender;
 import java.util.Map;
 
 public class Inventory {
-    Map<Product, Integer> refill;
-
+    Map<Output, Integer> tracker;
+    void add(Output output){
+        int count = tracker.get(output);
+        tracker.put(output, count + 1);
+    }
 }
--- src/main/java/vender/VendingMachine.java
+++ src/main/java/vender/VendingMachine.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 class VendingMachine {
     int capcity;
-
+    Inventory inventory;
     Output takeOrder(Order order){
         List<Output> outputs = Arrays.asList();
         outputs.add(order);
