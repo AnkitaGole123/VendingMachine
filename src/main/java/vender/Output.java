aab6deb249782ff4d450837c6a5cddcea303aeb1
Ankitagole <ankita17@navgurukul.org>
2019-09-12 16:41:12 +0530

    add. Output class

---
+++ src/main/java/vender/Output.java
@@ -0,0 +1,15 @@
+package vender;
+
+public class Output {
+
+    private String product;
+    private double remainingChange;
+
+    public String getProduct() {
+        return product;
+    }
+
+    public double getRemainingChange() {
+        return remainingChange;
+    }
+}
