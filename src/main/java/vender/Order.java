f6bca9a2229ac7a8f85ac6cd9ccb6304403a15bb
Ankitagole <ankita17@navgurukul.org>
2019-09-12 15:22:50 +0530

    add. constructor and getter in product class

--- src/main/java/vender/Order.java
+++ src/main/java/vender/Order.java
@@ -4,4 +4,21 @@ public class Order {
     private Product name;
     private Coins price;
     double quantity;
+    public Order(Product name, Coins price, double quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public Product getName() {
+        return name;
+    }
+
+    public Coins getPrice() {
+        return price;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
 }
